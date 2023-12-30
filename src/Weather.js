472ffd3c0b7252e48ebcc48e010f4db6a8fb9b24
elenvlas <argex@ukr.net>
2023-12-30 08:57:38 +0200

    Changed the loader to orange

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
   visible={true}
   height="80"
   width="80"
-  color="#4fa94d"
+  color="orange"
   ariaLabel="puff-loading"
   wrapperStyle={{}}
   wrapperClass=""
