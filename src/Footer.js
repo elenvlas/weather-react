e06057fb8cdb169115ae315bfe17dc666179ce1f
elenvlas <argex@ukr.net>
2024-01-02 00:12:59 +0200

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div>
       <small>
-        <a href="https://github.com/elenvlas/weather-react" target="_blank">Open-source code</a>
+        <a href="https://github.com/elenvlas/weather-react" target="_blank" rel="noreferrer"> Open-source code</a>
         , by  <a href="https://www.elenavlasenko.io">Elena Vlasenko</a>  from  <a href="https://elena.io/">Elena.io</a>
       </small>
     </div>
