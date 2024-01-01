c2d96d4b649a9410a8aaa70ec6cd26e6615370ed
elenvlas <argex@ukr.net>
2024-01-01 22:53:49 +0200

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Header() {
   return <ul className="list-city row row-cols-auto row-city">
           <li className="col-2"><a href="/">Lisbon</a></li>
-          <li className="col-2"><a href="/">Paris</a></li>
-          <li className="col-2"><a href="/">Sydney</a></li>
-          <li className="col"><a href="/">San Francisco</a></li>
+          <li className="col-2"><a href="/paris">Paris</a></li>
+          <li className="col-2"><a href="/sydney">Sydney</a></li>
+          <li className="col"><a href="/san-francisco">San Francisco</a></li>
         </ul>
 }
